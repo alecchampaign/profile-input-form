4d63f02d360c2d19b9961b0ea6c849078f58b40d
Alec Champaign <alecchampaign@protonmail.com>
2020-02-23 21:01:27 -0500

    Init

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [BrowserModule, ReactiveFormsModule, RouterModule.forRoot([])],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
